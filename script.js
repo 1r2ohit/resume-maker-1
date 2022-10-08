fc1eb2f2eec023e2e7b82c95414582499a7367f5
Aniket Sharma <aniket.sharma.4509@gmail.com>
2022-10-08 14:22:27 +0530

    first commit

--- script.js
+++ script.js
@@ -77,20 +77,10 @@ $(document).ready(() => {
   }
 
   $('#genrate').click(() => {
-    $('#main-resume').printThis({
-      style: true,
-      importCSS: true,
-      copyTagClasses: true,
-      beforePrint: () => {
-        $('#main-resume').css('font-size', '200%');
-        $('#main-resume h1').css('font-size', '250%');
-      }, // function called before iframe is filled
-      afterPrint: () => {
-        $('#main-resume').css('font-size', '100%');
-      },
-    });
+    CreatePDFfromHTML();
   });
 });
+
 
 function setTheme(darkColour, lightColour) {
   $('.my-bg-primary').css('background-color', darkColour);
@@ -190,3 +180,41 @@ $('#experience-section').click(() => {
 $('#img-col').click(() => {
   $('#form-img input[type=file]').click();
 });
+
+function CreatePDFfromHTML() {
+  var HTML_Width = $('#main-resume').width();
+  var HTML_Height = $('#main-resume').height();
+  var top_left_margin = 15;
+  var PDF_Width = HTML_Width + top_left_margin * 2;
+  var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
+  var canvas_image_width = HTML_Width;
+  var canvas_image_height = HTML_Height;
+
+  var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+  html2canvas($('#main-resume')[0]).then(function (canvas) {
+    var imgData = canvas.toDataURL('image/jpeg', 1.0);
+    var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+    pdf.addImage(
+      imgData,
+      'JPG',
+      top_left_margin,
+      top_left_margin,
+      canvas_image_width,
+      canvas_image_height
+    );
+    for (var i = 1; i <= totalPDFPages; i++) {
+      pdf.addPage(PDF_Width, PDF_Height);
+      pdf.addImage(
+        imgData,
+        'JPG',
+        top_left_margin,
+        -(PDF_Height * i) + top_left_margin * 4,
+        canvas_image_width,
+        canvas_image_height
+      );
+    }
+    pdf.save('Your_PDF_Name.pdf');
+    // $('#main-resume').hide();
+  });
+}
